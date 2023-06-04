abce1feab71a0c9ad2b12819e213cf20c6ec7261
Vladimir Burkov <Vladimir.Burkov@outlook.com>
2023-06-04 18:29:32 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/ards-dev-public/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
